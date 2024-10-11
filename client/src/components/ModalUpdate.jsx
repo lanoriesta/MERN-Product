c2e1d94ca13c0d400cebccccb846b347ef8100ba
Lawrence Angelo Noriesta <lanoriesta@gmail.com>
2024-10-12 06:43:35 +0800

    add update product function and able to connect and use it on edit btn

--- client/src/components/ModalUpdate.jsx
+++ client/src/components/ModalUpdate.jsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Heading,
   FormControl,
   HStack,
   Input,
@@ -11,37 +12,109 @@ import {
   ModalHeader,
   ModalOverlay,
   useColorModeValue,
-  useDisclosure,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React from "react";
+import { useState } from "react";
+import { useProductStore } from "../store/products";
 
 const ModalUpdate = ({ isOpen, onClose, productData }) => {
   const bgModal = useColorModeValue("#F3F3F1", "hsl(0,0%,10%)");
+  const [updatedProduct, setUpdatedProduct] = useState(productData);
+  const { updateProduct } = useProductStore();
+  const toast = useToast();
+
+  const handleUpdateProduct = async (id, product) => {
+    const { success, message } = await updateProduct(id, product);
+    if (success) {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+        position: "bottom-left",
+        variant: "left-accent",
+      });
+    } else {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "bottom-left",
+        variant: "left-accent",
+      });
+    }
+    onClose();
+  };
   return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      size={"lg"}
+      motionPreset="slideInBottom"
+    >
       <ModalOverlay backdropFilter="blur(5px)" />
       <ModalContent bg={bgModal}>
-        <ModalHeader>{productData.name}</ModalHeader>
+        <ModalHeader py={10}>
+          <Heading
+            as={"h1"}
+            fontSize={"2xl"}
+            textAlign={"center"}
+            textTransform={"uppercase"}
+            letterSpacing={10}
+          >
+            Update <span style={{ color: "#EA553C" }}>Product</span>
+          </Heading>
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           <form>
             <FormControl>
               <VStack gap={5}>
                 <Input
-                  value={productData.name}
+                  px={3}
                   variant={"flushed"}
                   focusBorderColor="#EA553C"
+                  value={updatedProduct.name}
+                  name="name"
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      name: e.target.value,
+                    })
+                  }
                 />
                 <Input
-                  value={productData.price}
+                  px={3}
+                  type="number"
                   variant={"flushed"}
                   focusBorderColor="#EA553C"
+                  name="price"
+                  value={updatedProduct.price}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      price: e.target.value,
+                    })
+                  }
                 />
                 <Input
-                  value={productData.image}
+                  px={3}
                   variant={"flushed"}
                   focusBorderColor="#EA553C"
+                  name="image"
+                  value={updatedProduct.image}
+                  onChange={(e) =>
+                    setUpdatedProduct({
+                      ...updatedProduct,
+                      image: e.target.value,
+                    })
+                  }
                 />
               </VStack>
             </FormControl>
@@ -59,6 +132,9 @@ const ModalUpdate = ({ isOpen, onClose, productData }) => {
               my={3}
               color={"white"}
               boxShadow={"base"}
+              onClick={() =>
+                handleUpdateProduct(productData._id, updatedProduct)
+              }
             >
               Update
             </Button>
