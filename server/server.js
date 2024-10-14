4fc5c702482d263d65344951e2cfcd45993c73fb
Lawrence Angelo Noriesta <lanoriesta@gmail.com>
2024-10-14 20:58:26 +0800

    Add dev, build and start in script for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -8,10 +9,20 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
+const __dirname = path.resolve();
+
 // Middleware
 app.use(express.json()); // Allows us to accept JSON data in the body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
