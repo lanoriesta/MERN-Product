d3fb192519e56d6b309694eeabee7273b19592a2
Lawrence Angelo Noriesta <lanoriesta@gmail.com>
2024-10-14 21:22:24 +0800

    add PORT in .env file

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
